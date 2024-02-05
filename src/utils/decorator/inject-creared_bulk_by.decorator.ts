ec6b3f8a2879f7cf9c73d5c3bece90fde78b0a7d
zakyraihan <zakyraihan6336@gmail.com>
2024-02-05 09:20:54 +0700

    change

--- src/utils/decorator/inject-creared_bulk_by.decorator.ts
+++ src/utils/decorator/inject-creared_bulk_by.decorator.ts
@@ -4,7 +4,16 @@ export const InjectCreatedBulkBy = createParamDecorator(
   (data: any, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
 
-    req.body.created_by = req.user.id;
+    const test = req.body.data.map((items) => {
+      return {
+        ...items,
+        created_by: {
+          id: req.user.id,
+        },
+      };
+    });
+
+    req.body.data = test;
 
     return req.body;
   },
